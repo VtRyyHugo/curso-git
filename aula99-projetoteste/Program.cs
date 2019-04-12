3d788a0174e130e143e5806fe96a66b5c50a79fa
Vitor Hugo <vitorhugo929@gmail.com>
2019-04-12 15:03:57 -0300

    Projeto iniciado, mostrando Hello World

---
+++ aula99-projetoteste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace aula99_projetoteste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
