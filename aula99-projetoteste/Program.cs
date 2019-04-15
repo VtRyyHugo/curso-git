aac2766e5c4151fc8f53bdc5ae9a9057c9f69914
Vitor Hugo <vitorhugo929@gmail.com>
2019-04-15 11:51:00 -0300

    Acrescentando linhas

--- aula99-projetoteste/Program.cs
+++ aula99-projetoteste/Program.cs
@@ -6,6 +6,7 @@ namespace aula99_projetoteste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("-------------------------------------");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Good morning!");
             Console.WriteLine("Have a good day!");
